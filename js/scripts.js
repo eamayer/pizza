b570409139d9d6cd39f353922992e4b9aad1ab04
eamayer <elizabeth.a.mayer@email.com>
2019-07-14 20:56:33 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Pizza Constructor
+function Pizza(size) {
+  this.pizzaSize = size;
+  this.pizzaToppings = [];
+}
+
+// Pizza Prototypes
+Ticket.prototype.price = function() {
+    var price = 20;
+
+    if (this.pizzaSize === "large") {
+      price += 10;
+    } else if (this.pizzaSize === "medium") {
+      price += 5;
+    } else {
+      price
+    }
+
+    if (this.pizzaToppings.length === 0) {
+      price
+    } else {
+      price += this.pizzaToppings.legth
+    }
+
+  return price
+
+};
+
+
+
+// User Interface Logic ---------
+
+
+$(document).ready(function() {
+  var totalPrice = 0;
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    var pizzaOrder = $("select#pizza-size").val();
+    var newPizza = new Pizza(pizzaOrder)
+
+    .forEach($("input[name='toppings']:checked"), function() {
+     newPizza.pizzaToppings.push($(this).val());
+   });
+
+
+
+    $("#show-price").show();
+    $("ul#ticketInCart").append("<li>" + "$" + ticketPrice + "</li>");
+
+  });
+})
